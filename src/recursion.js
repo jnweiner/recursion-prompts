2d349ab8dec7de9e894d02eb1fa95f4768ad8ebf
Jordan Weiner <weinerj8@gmail.com>
2020-09-11 16:29:29 -0700

    Partially implement modulo, except for negative nums.

--- src/recursion.js
+++ src/recursion.js
@@ -137,7 +137,18 @@ var palindrome = function(string) {
 // modulo(5,2) // 1
 // modulo(17,5) // 2
 // modulo(22,6) // 4
+  // keep decrementing x by y until x < y
 var modulo = function(x, y) {
+  if (y === 0) {
+    return NaN;
+  }
+  if (x === y) {
+    return 0;
+  }
+  if (x < y) {
+    return x;
+  }
+  return modulo(x - y, y);
 };
 
 // 12. Write a function that multiplies two numbers without using the * operator or
