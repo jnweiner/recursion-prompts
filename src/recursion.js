3b8690d31bfd53d55a6875bf27748264c77451e8
Jordan Weiner <weinerj8@gmail.com>
2020-09-11 21:08:01 -0700

    Implemented FizzBuzz, all tests passing

--- src/recursion.js
+++ src/recursion.js
@@ -258,6 +258,20 @@ var buildList = function(value, length) {
 // For numbers which are multiples of both three and five, output “FizzBuzz” instead of the number.
 // fizzBuzz(5) // ['1','2','Fizz','4','Buzz']
 var fizzBuzz = function(n) {
+  if (n - 1 === 0) {
+    return ['1'];
+  }
+  let array = fizzBuzz(n - 1);
+  if ((n % 3 === 0) && (n % 5 === 0)) {
+    array.push('FizzBuzz');
+  } else if (n % 3 === 0) {
+    array.push('Fizz');
+  } else if (n % 5 === 0) {
+    array.push('Buzz');
+  } else {
+    array.push(n.toString());
+  }
+  return array;
 };
 
 // 20. Count the occurence of a value in a list.
