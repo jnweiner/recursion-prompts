c755588013eea4ea78828ee7cea86f410de35258
Jordan Weiner <weinerj8@gmail.com>
2020-09-12 15:22:30 -0700

    Implemented countValuesInObj, all tests passing

--- src/recursion.js
+++ src/recursion.js
@@ -321,6 +321,16 @@ var countKeysInObj = function(obj, key) {
 // countValuesInObj(obj, 'r') // 2
 // countValuesInObj(obj, 'e') // 1
 var countValuesInObj = function(obj, value) {
+  let num = 0;
+  for (let key in obj) {
+    if (obj[key] === value) {
+      num += 1;
+    }
+    if (typeof obj[key] === 'object') {
+      num += countValuesInObj(obj[key], value);
+    }
+  }
+  return num;
 };
 
 // 24. Find all keys in an object (and nested objects) by a provided name and rename
